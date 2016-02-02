ddac0180699e743cabed788e20cd5849e3b353ff
Bruno C <mail@brunocartier.fr>
2016-02-02 20:56:28 +0100

    Send morning & evening averages

--- ride_or_drive.py
+++ ride_or_drive.py
@@ -13,8 +13,9 @@ from config import SMS_USER, SMS_KEY, WEATHER_KEY
 SMS_URL = 'https://smsapi.free-mobile.fr/sendmsg'
 WEATHER_URL = 'http://api.wunderground.com/api/{}/hourly/q/FR/La_Rochelle.json'.format(WEATHER_KEY)
 MESSAGE = 'Probabilités de pluie :\n' \
-          '9h : {}% - 10h : {}% - 11h : {}%\n' \
-          '17h : {}% - 18h : {}% - 19h : {}%'
+          '9h={}%, 10h={}%, 11h={}%\n' \
+          '17h={}%, 18h={}%, 19h={}%\n' \
+          'Donc, matin={}%, aprèm={}%.'
 
 
 def send_sms(text):
@@ -48,17 +49,31 @@ def fetch_weather():
 
                 if hour in wanted_hours and hour not in out:
                     out[hour] = hour_info['pop']
-            send_sms(MESSAGE.format(
-                out['9'],
-                out['10'],
-                out['11'],
-                out['17'],
-                out['18'],
-                out['19'],
-            ))
+
+            return out
     except HTTPError as e:
         send_sms('Désolé, y\' eu une erreur avec la requête... - {}'.format(e.reason))
+        return
+
+
+def send_weather(weather):
+    morning = [int(weather['9']), int(weather['10']), int(weather['11'])]
+    morning_average = round(sum(morning) / len(morning), 2)
+
+    evening = [int(weather['17']), int(weather['18']), int(weather['19'])]
+    evening_average = round(sum(evening) / len(evening), 2)
+
+    send_sms(MESSAGE.format(
+        weather['9'],
+        weather['10'],
+        weather['11'],
+        weather['17'],
+        weather['18'],
+        weather['19'],
+        morning_average,
+        evening_average
+    ))
 
 
 if __name__ == '__main__':
-    fetch_weather()
+    send_weather(fetch_weather())
